869a3ab64333204b84ebd5337d42a7d128e5bc7e
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2022-06-25 01:07:43 +0600

    last commit

---
+++ kalkulator.js
@@ -0,0 +1,22 @@
+// “Калькулятор” - пользователь вводит с клавиатуры число, символ арифметического действия (+, -, *, /) и еще одно число, Скрипт должен вывести результат 
+
+// let nummer = +prompt("введите первое число");
+// let symbol = prompt("введите операцию", "+");
+// let nummerTwo = +prompt("введите второе число");
+
+// if (symbol === "+"){
+//     console.log(nummer + nummerTwo);
+// }
+// else if (symbol === "-"){
+//     console.log(nummer - nummerTwo);
+// }
+// else if (symbol === "*"){
+//     console.log(nummer * nummerTwo);
+// }
+// else if (symbol === "/"){
+//     console.log(nummer / nummerTwo);
+// }
+// else {
+//     console.log("Вы ввели не правильно. Попробуйте ещё раз");
+// }
+
